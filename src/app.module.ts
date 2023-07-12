8b16bc37cccbbf370cf1872f84d336e810e3d8d4
Pradeep_Ranasinghe <pradeep@climatesi.com>
2023-07-12 15:36:17 +0530

    merge complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { Sector } from './entity/sector.entity';
       // password: 'password',
       //database: 'nccdsndb',
       username: 'root',
-      password: 'pradeep123#',
+      password: '',
       database: 'icat_pmu',
       entities: [Applicability,
         Country,
